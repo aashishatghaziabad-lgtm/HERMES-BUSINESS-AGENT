fe9d5d3eca86b688d74793c6af691bae1f352fd1
aashishatghaziabad-lgtm <aashishatghaziabad@gmail.com>
2026-09-16 00:10:54 -0700

    index.js

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ export default {
 
       try {
         const response = await fetch(
-          "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent",
+          "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash-lite:generateContent",
           {
             method: "POST",
             headers: {
